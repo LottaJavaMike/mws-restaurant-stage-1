9cab574de0a6ceeb964e1803327347471590edd0
Mike Shea <emtshea@gmail.com>
2018-09-23 17:21:35 -0400

    add tabindex

--- js/main.js
+++ js/main.js
@@ -178,6 +178,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  // tab index for accessibility
+  more.tabindex = '3';
   li.append(more)
 
   return li

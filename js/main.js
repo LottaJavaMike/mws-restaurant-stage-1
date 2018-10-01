897d85d759fe35b57658971e323da5bf9c29fb60
Mike Shea <emtshea@gmail.com>
2018-09-30 22:38:46 -0400

    removed mapbox token

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZW10c2hlYSIsImEiOiJjam02YXozNGUwYnAzM3N1bnFud2JyMTNxIn0.Qz_a-kN2s83EDG-oHLLT6Q',
+    mapboxToken: '',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
